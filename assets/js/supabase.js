f2c01a8dff318ae2337fe96783be8da1a01120d7
Tiago <tiagoandresn@gmail.com>
2024-08-17 15:26:43 +0100

    fim dos testes

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = data.SUPABASE_URL;
-const SUPABASE_ANON_KEY = data.SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://lfgxpcrwtpzmaswdwvpi.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmZ3hwY3J3dHB6bWFzd2R3dnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzNzc3MTcsImV4cCI6MjAzNzk1MzcxN30.lF1lVs4yvs_rsxXpo-mN91AWSzNPYWdQUeihM_B8pFk";
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let currentPage = 0;
